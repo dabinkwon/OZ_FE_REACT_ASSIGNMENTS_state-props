072f703695e3442b12f304387f09f116b18013d8
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-08-08 11:16:58 +0900

    state와 props를 사용하여 카운터 앱 생성하기 과제 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
-import './App.css';
-import CountResult from './CountResult';
+import { useState } from "react";
+import "./App.css";
+import CountResult from "./CountResult";
 
 function App() {
   // 1.useState를 사용하여 상태를 활용하여 기능을 구현하세요.
+  const [counter, setCounter] = useState(0);
   let count = 0;
+  const incrementCounter = () => {
+    setCounter(counter + 1);
+  };
+  const decrementCounter = () => {
+    setCounter(counter - 1);
+  };
   return (
     <>
       <h1>⏰ 카운트 앱 생성하기 </h1>
       <p>버튼을 누를 때 마다 버튼이 실시간으로 변경되도록 구현해 주세요.</p>
-      <h2>count: {count}</h2>
-      <button>+</button>
-      <button>-</button>
+      <h2>count : {count}</h2>
+      <PlusButton incrementCounter={incrementCounter} />
+      <MinusButton decrementCounter={decrementCounter} />
       {/* 2. 상태로 관리하고 있는 count 값을 props를 통해 CountResult 컴포넌트로 전달하세요. */}
-      <CountResult />
+      <CountResult counter={counter} />
     </>
   );
+}
+function PlusButton({ incrementCounter }) {
+  return <button onClick={incrementCounter}>+</button>;
+}
+function MinusButton({ decrementCounter }) {
+  return <button onClick={decrementCounter}>-</button>;
 }
 
 export default App;

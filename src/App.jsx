8a6b44e6bf524a1ff4c9b00856d0bc351cd8dd93
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-08-11 16:39:07 +0900

    과제 피드백 반영

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import CountResult from "./CountResult";
 function App() {
   // 1.useState를 사용하여 상태를 활용하여 기능을 구현하세요.
   const [counter, setCounter] = useState(0);
-  let count = 0;
   const incrementCounter = () => {
     setCounter(counter + 1);
   };
@@ -16,7 +15,7 @@ function App() {
     <>
       <h1>⏰ 카운트 앱 생성하기 </h1>
       <p>버튼을 누를 때 마다 버튼이 실시간으로 변경되도록 구현해 주세요.</p>
-      <h2>count : {count}</h2>
+      <h2>count : {counter}</h2>
       <PlusButton incrementCounter={incrementCounter} />
       <MinusButton decrementCounter={decrementCounter} />
       {/* 2. 상태로 관리하고 있는 count 값을 props를 통해 CountResult 컴포넌트로 전달하세요. */}
